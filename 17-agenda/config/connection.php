0d39c2b46c80168dc60fb6bd25d50ac055e8729b
jacksonAbrao <jackson.comp1681@hotmail.com>
2022-03-17 00:34:40 -0300

    .

--- 17-agenda/config/connection.php
+++ 17-agenda/config/connection.php
@@ -1,8 +1,8 @@
 <?php
-    $host = 'localhost';
-    $dbname = 'agenda';
-    $user = 'root';
-    $pass = '';
+    $host = "localhost";
+    $dbname = "agenda";
+    $user = "root";
+    $pass = "";
 
     try {
         $conn = new PDO("mysql:host=$host;dbname=$dbname", $user, $pass);
@@ -11,5 +11,5 @@
         $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         
     } catch (PDOException $e) {
-        echo 'Erro ao conectar com o banco de dados: ' . $e->getMessage();
+        echo "Erro ao conectar com o banco de dados: " . $e->getMessage();
     }
--- 17-agenda/config/process.php
+++ 17-agenda/config/process.php
@@ -4,12 +4,34 @@
     include_once("config/connection.php");
     include_once("config/url.php");
 
-    $contacts = [];
+    $id;
 
-    $query = "SELECT * FROM contacts";
+    if(!empty($_GET)) {
+        $id = $_GET["id"];
+    }
 
-    $stmt = $conn->prepare($query);
+    //retorna o dado de um contato
+    if (!empty($id)) {
+        
+        $query = "SELECT * FROM contacts WHERE id = :id";
 
-    $stmt->execute();
+        $stmt = $conn->prepare($query);
+
+        $stmt->bindParam(":id", $id);
+
+        $stmt->execute();
+
+        $contact = $stmt->fetch(PDO::FETCH_ASSOC);
+    } else {
+
+        //retorna todos os contatos
+        $contacts = [];
     
-    $contacts = $stmt->fetchAll();
+        $query = "SELECT * FROM contacts";
+    
+        $stmt = $conn->prepare($query);
+    
+        $stmt->execute();
+        
+        $contacts = $stmt->fetchAll();
+    }
